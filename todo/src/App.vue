3ee31452ea0c5f2a1241db322699f20f5c385e48
teawon <xodnjsvkvk9@gmail.com>
2024-01-07 21:28:56 +0900

    feat : 라우팅 설정

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,19 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
-
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
-
-@Options({
-  components: {
-    HelloWorld,
-  },
-})
-export default class App extends Vue {}
-</script>
 
 <style>
 #app {
