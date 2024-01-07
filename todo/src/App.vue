3769f2dcc1cd6c177021f5ae8211bd17a334c9b2
teawon <xodnjsvkvk9@gmail.com>
2024-01-07 21:57:36 +0900

    feat : globalCSS 적용

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -5,6 +5,17 @@
 </template>
 
 <style>
+/* global CSS */
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-image: url("./assets/background.gif"); /* 배경 이미지 경로 설정 */
+  background-size: cover;
+  background-position: center;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
